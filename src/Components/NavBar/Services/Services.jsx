d7b2269d1afdd19557a8466c963d6ecb6f6f282c
MarkoAyala <markoayala3@hotmail.com>
2022-06-01 13:15:17 -0300

    services finish

--- src/Components/NavBar/Services/Services.jsx
+++ src/Components/NavBar/Services/Services.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from '../../../img/heartemoji.png';
 import Glasses from '../../../img/glasses.png';
 import Humble from '../../../img/humble.png';
 import Card from '../Card/Card';
+import CV from '../../../img/MARKOAYALA-CV-FULLSTACK.pdf'
 
 function Services() {
   return (
@@ -13,27 +14,38 @@ function Services() {
             <span>Ofrezco los</span>
             <span>siguientes servicios:</span>
             <span>lorem ipmsum dor loriam no se que escribir en esta <br /> parte del cv ahre loco lorem impsu dor</span>
+            <a href={CV} download>
             <button className='button s-button'>Descargar CV</button>
+            </a>
+            <div className="blur s-blur" style={{background:"#ABF1FF94"}}></div>
             <div className="blur s-blurl" style={{background:"#ABF1FF94"}}></div>
         </div>
         {/* right side */}
         <div className="cards">
           {/* first card */}
-            <div style={{left:'20rem'}}>
+            <div style={{left:'24rem'}}>
                 <Card
                 emoji={HeartEmoji}
                 heading={'Back-end'}
-                detail={'MongoDB , Sequelize , PostgreSQL , TypeScript'}
+                detail={'MongoDB, Express, Sequelize, PostgreSQL, TypeScript'}
                 />
             </div>
             {/* second card */}
-          <div style={{top:"12rem", left: "-4rem"}}>
+          <div style={{top:"12rem", left: "-1rem"}}>
           <Card
                 emoji={Glasses}
                 heading={'Front-end'}
                 detail={'React, JS , TS , Redux Toolkit, MaterialUI...'}
                 />
           </div>
+          <div style={{top:"22rem", left:"20rem"}}>
+          <Card
+                emoji={Humble}
+                heading={'Soft skils'}
+                detail={'Trabajo en equipo, Aprendizaje constate e indepediente, Creativo'}
+                />
+          </div>
+          <div className='blur s-blur2' style={{background: 'var(--purple)'}} ></div>
         </div>
     </div>
   )
