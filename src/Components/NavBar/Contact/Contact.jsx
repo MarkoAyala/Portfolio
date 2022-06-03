41057689476df52fc845c91cdf58e3fa52a8dd88
MarkoAyala <markoayala3@hotmail.com>
2022-06-03 17:44:56 -0300

    emailjs implementado

--- src/Components/NavBar/Contact/Contact.jsx
+++ src/Components/NavBar/Contact/Contact.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import './Contact.css'
+import React, { useRef } from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_yod69ko', 'template_b7zvoh9', form.current, 'ZyuuO7ufP--eLgvbI')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
   return (
     <div className="contact-form">
         <div className="w-left">
             <div className="awesome">
-                <span>Ponte en contacto</span>
-                <span>Contactame</span>
+                <span>Ponte en contacto,</span>
+                <span>estoy disponible!</span>
                 <div className="blur s-blur" style={{background:'#ABF1FF94'}}></div>
             </div>
         </div>
         <div className="c-right">
-            <form action="">
+            <form ref={form} onSubmit={sendEmail}>
                 <input type="text" name="user_name" className='user' placeholder='Nombre' />
                 <input type="email" name="user_email" className='user' placeholder='Mail' />
                 <textarea name="message" className='user' placeholder='Mensaje' />
