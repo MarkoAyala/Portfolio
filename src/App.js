3544fbc38faf4876b9f051be43641cf226264362
MarkoAyala <markoayala3@hotmail.com>
2022-06-05 20:48:50 -0300

    implementación del footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Services from "./Components/NavBar/Services/Services";
 import Experience from "./Components/NavBar/Experience/Experience";
 import Portfolio from "./Components/NavBar/Portfolio/Portfolio";
 import Contact from "./Components/NavBar/Contact/Contact";
+import Footer from "./Components/NavBar/Footer/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Experience/>
       <Portfolio/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
