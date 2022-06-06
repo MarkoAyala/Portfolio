a69c6a2aa3d3a7851ff61b6ed370a87e19a1c2d9
MarkoAyala <markoayala3@hotmail.com>
2022-06-06 17:24:07 -0300

    framer-motion animation end

--- src/Components/NavBar/Services/Services.jsx
+++ src/Components/NavBar/Services/Services.jsx
@@ -7,10 +7,12 @@ import Card from '../Card/Card';
 import CV from '../../../img/MARKOAYALA-CV-FULLSTACK.pdf';
 import { themeContext } from '../../../Context';
 import { useContext } from "react";
+import {motion} from 'framer-motion'
 
 function Services() {
   const theme=useContext(themeContext);
   const darkMode=theme.state.darkMode;
+  const transition ={duration:1, type:'spring'}
   return (
     <div className="services">
         {/* left side */}
@@ -27,28 +29,41 @@ function Services() {
         {/* right side */}
         <div className="cards">
           {/* first card */}
-            <div style={{left:'24rem'}}>
+            <motion.div
+            initial={{left:'16rem'}}
+            transition={transition}
+            whileInView={{left:'25rem'}}
+            >
                 <Card
                 emoji={HeartEmoji}
                 heading={'Back-end'}
                 detail={'MongoDB, Express, Sequelize, PostgreSQL, TypeScript'}
                 />
-            </div>
+            </motion.div>
             {/* second card */}
-          <div style={{top:"12rem", left: "-1rem"}}>
+          <motion.div style={{top:"12rem", left: "-1rem"}}
+          
+          initial={{ left: "-10rem", top: "12rem" }}
+          whileInView={{ left: "-3rem" }}
+          transition={transition}
+          >
           <Card
                 emoji={Glasses}
                 heading={'Front-end'}
                 detail={'React, JS , TS , Redux Toolkit, MaterialUI...'}
                 />
-          </div>
-          <div style={{top:"22rem", left:"20rem"}}>
+          </motion.div>
+          <motion.div style={{top:"22rem", left:"25rem"}}
+           initial={{ top: "22rem", left: "25rem" }}
+           whileInView={{ left: "20rem" }}
+           transition={transition}
+          >
           <Card
                 emoji={Humble}
                 heading={'Soft skils'}
                 detail={'Trabajo en equipo, Aprendizaje constate e indepediente, Creativo'}
                 />
-          </div>
+          </motion.div>
           <div className='blur s-blur2' style={{background: 'var(--purple)'}} ></div>
         </div>
     </div>
