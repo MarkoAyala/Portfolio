631acd90d2f962f2830898abbc6715236fdbd2ff
MarkoAyala <markoayala3@hotmail.com>
2022-06-06 16:00:18 -0300

    toggle finish

--- src/Components/NavBar/Experience/Experience.jsx
+++ src/Components/NavBar/Experience/Experience.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import './Experience.css';
+import { themeContext } from '../../../Context';
+import { useContext } from "react";
 
 function Experience() {
+  const theme=useContext(themeContext);
+  const darkMode=theme.state.darkMode;
   return (
     <div className="experience">
         <div className="blura" style={{ background: 'rgb(238,210,255)'}}></div>
         <div className="bluras" style={{ background: '#C1F5FF', top:"8rem", width:"21rem", height:'11rem', left:"50rem"}}></div>
         <div className="achievement">
-            <div className="circle">1+</div>
+            <div className="circle"  style={{backgroundImage:darkMode?'linear-gradient(to bottom, #ff0000 0%, #2500ff 100%)'
+: ''}}>1+</div>
             <span>Año/s</span>
             <span>Programando</span>
         </div>
         <div className="achievement">
-            <div className="circle">4+</div>
+            <div className="circle" style={{backgroundImage:darkMode?'linear-gradient(to bottom, #ff0000 0%, #2500ff 100%)'
+: ''}}>4+</div>
             <span>Proyectos</span>
             <span>Terminados</span>
         </div>
         <div className="achievement">
-            <div className="circle">20+</div>
+            <div className="circle"  style={{backgroundImage:darkMode?'linear-gradient(to bottom, #ff0000 0%, #2500ff 100%)'
+: ''}}>20+</div>
             <span>Tecnologias</span>
             <span>Dominadas</span>
         </div>
