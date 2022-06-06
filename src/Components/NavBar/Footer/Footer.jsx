29e9ff90b8a803f4ea968d38c76ac6355f1cc559
MarkoAyala <markoayala3@hotmail.com>
2022-06-05 21:10:46 -0300

    footer finish

--- src/Components/NavBar/Footer/Footer.jsx
+++ src/Components/NavBar/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './Footer.css';
+import Github from '@iconscout/react-unicons/icons/uil-github';
+import Footeri from '../../../img/wave.png'
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+        <img src={Footeri} alt="" style={{width:'100%'}}/>
+        <div className="f-content">
+            <span>marko.ayala.dev99@hotmail.com</span>
+            <div className="f-icons">
+                <a href="https://github.com/MarkoAyala" target='_blank'><Github color='black' size="3rem"/></a> 
+            </div>
+        </div>
+    </div>
   )
 }
 
